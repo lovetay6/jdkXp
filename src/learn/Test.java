7496c7faa5ec73c958c7c99560d4e61d85a18088
lovetay6 <643795238@qq.com>
2023-03-12 14:20:21 +0800

    jdk源码解析

---
+++ src/learn/Test.java
@@ -0,0 +1,12 @@
+package learn;
+
+import java.util.HashMap;
+
+public class Test {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("love","lost");
+        System.out.println(map.get("love"));
+    }
+}
